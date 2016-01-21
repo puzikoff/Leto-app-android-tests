98c2849a77fe6bc9f717e97739927332d7d2cadf
ChepelVE <chepelve@ekassir.com>
2016-01-21 16:28:59 +0300

    Excel test data provider

--- src/test/java/ru/letoapp/tests/DevelopTest.java
+++ src/test/java/ru/letoapp/tests/DevelopTest.java
@@ -13,7 +13,7 @@ public class DevelopTest extends SetUpForSuiteBase {
 		greetingPopupHandler();
 		appManager.getAuthScreen().verifyAuthScreen();
 		appManager.getAuthScreen().enterUsername(
-				PropertyReader.getProperty("cardUsername"));
+				PropertyReader.getProperty("devUsername"));
 		appManager.getAuthScreen().enterPassword(
 				PropertyReader.getProperty("password"));
 		protectCodeCheckboxUnckeck();
@@ -27,21 +27,9 @@ public class DevelopTest extends SetUpForSuiteBase {
 
 	@Test(priority = 2, dependsOnMethods = { "auth" })
 	public void openCardTest() throws Exception {
-		appManager.getDashboardScreen().paymentsBtnClik();
-		appManager.getPaymentsAndTransfersScreen().paymentsTabBtnClick();
-		appManager.getPaymentsAndTransfersScreen().getPaymentsTab()
-				.categoryClick("Мобильные операторы");
-		appManager.getPaymentsAndTransfersScreen().getPaymentsTab()
-				.paymentClick("МТС");
-		appManager.getMobilePaymentScreen().enterAccount("7777777777");
-		appManager.getMobilePaymentScreen().enterAmount("23");
-		appManager.getMobilePaymentScreen()
-				.waitForVanishCommissionRecalculateSpiner();
-		appManager.getMobilePaymentScreen().turnSaveTemplateSwitch();
-		appManager.getMobilePaymentScreen().paymentToolBtnClick();
-		appManager.getPaymentToolChoiceScreen().cancelBtnClick();
-		appManager.getMobilePaymentScreen().nextBtnClick();
-
+		appManager.getDashboardScreen().orderCardBtnClick();
+		appManager.getDashboardScreen().getTitleFromActionBar();
+		
 	}
 
 }
